6c265c508c2b1315bc9ab9d1e280cc890d980423
Kyle Thayer <kyle.thayer@gmail.com>
2021-12-07 11:49:08 -0800

    Initial commit

---
+++ exercise-1/exercise.R
@@ -0,0 +1,33 @@
+# Exercise-1
+# Developed from: http://tidytextmining.com/
+
+# Set up (install packages that you don't have)
+library(janeaustenr)
+library(tidytext)
+library(dplyr)
+library(stringr)
+library(ggplot2)
+
+# Load Jane Austen books into a dataframe using the austen_books() function
+
+
+# How many books are in the dataset?
+
+
+# Which book has the most lines?
+
+
+# Use the unnest_tokens function to generate the full list of words
+
+
+# Which words are most common (regardless of which book them come from)?
+
+
+# Remove stop words by performing an anti_join with the stop_words dataframe
+
+
+# Which non stop-words are most common?
+
+
+# Use ggplot to make a horizontal bar chart of the word frequencies of non-stop words
+
